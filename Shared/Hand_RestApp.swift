194f736ce187bbe66a63a4efb81d1836be2a43fb
TheBonnec <thomaslebonnec@icloud.com>
2022-02-16 11:03:46 +0100

    Initial Commit

---
+++ Shared/Hand_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hand_RestApp.swift
+//  Shared
+//
+//  Created by Thomas Le Bonnec on 16/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Hand_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
